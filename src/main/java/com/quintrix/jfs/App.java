4f6e8d45c81ae2508bd368b4044c35594c425070
Dustin Jones <dustinjonesswe@gmail.com>
2022-04-03 22:30:35 -0500

    Completed basic Hello World

---
+++ src/main/java/com/quintrix/jfs/App.java
@@ -0,0 +1,14 @@
+package com.quintrix.jfs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Dustin Jones - HW 1");
+    }
+}
