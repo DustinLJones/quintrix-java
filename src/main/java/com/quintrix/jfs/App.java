e8420f3a063deec3dfafbe1be1f45cc3297ff55b
Dustin Jones <dustinjonesswe@gmail.com>
2022-04-17 12:48:54 -0500

    Completed JDBC Practices

--- src/main/java/com/quintrix/jfs/App.java
+++ src/main/java/com/quintrix/jfs/App.java
@@ -10,60 +10,57 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	//Variables
-    	String hw = "Hello World!";
-    	String myName = "Dustin Jones";
-    	String fileName = "helloWorld.txt";
-    	String divider = "****************************************\n";
-    	String writeHead = divider + "* Writing to file\n" + divider;
-    	String readHead = divider + "* Reading from file\n" + divider;
-    	
-    	//Output hello word and name
-        System.out.println(hw);
-        System.out.println(myName);
-        System.out.println();
-        
-        
-        //Begin write process
-        //Output head
-        System.out.println(writeHead);
-        
-        //Attempt write
-        try {
-        	FileWriter writer = new FileWriter(fileName);
-        	
-        	writer.write(hw + " from " + myName);
-        	writer.close();
-        	
-        	System.out.println(" - Successfully executed \'write\' process\n\n");
-        	
-        } catch (IOException e) {
-        	System.out.println("Error with \'write\' process");
-        }
-        
-        //Begin read process
-        //Output head
-        System.out.println(readHead);
-        
-        //Attempt read process
-        try {
-        	File myFile = new File(fileName);
-        	Scanner fileReader = new Scanner(myFile);
-        	System.out.println(" - File contents: \n");
-        	
-        	while (fileReader.hasNextLine()) {
-        		System.out.println(fileReader.nextLine());
-        	}
-        	
-        	fileReader.close();
-        	System.out.println("\n - Successfully executed \'read\' process");
-        	
-        } catch (FileNotFoundException e) {
-        	System.out.println("Error with \'read\' process");
-        }
+public class App {
+  public static void main(String[] args) {
+    // Variables
+    String hw = "Hello World!";
+    String myName = "Dustin Jones";
+    String fileName = "helloWorld.txt";
+    String divider = "****************************************\n";
+    String writeHead = divider + "* Writing to file\n" + divider;
+    String readHead = divider + "* Reading from file\n" + divider;
+
+    // Output hello word and name
+    System.out.println(hw);
+    System.out.println(myName);
+    System.out.println();
+
+    // Begin write process
+    // Output head
+    System.out.println(writeHead);
+
+    // Attempt write
+    try {
+      FileWriter writer = new FileWriter(fileName);
+
+      writer.write(hw + " from " + myName);
+      writer.close();
+
+      System.out.println(" - Successfully executed \'write\' process\n\n");
+
+    } catch (IOException e) {
+      System.out.println("Error with \'write\' process");
     }
+
+    // Begin read process
+    // Output head
+    System.out.println(readHead);
+
+    // Attempt read process
+    try {
+      File myFile = new File(fileName);
+      Scanner fileReader = new Scanner(myFile);
+      System.out.println(" - File contents: \n");
+
+      while (fileReader.hasNextLine()) {
+        System.out.println(fileReader.nextLine());
+      }
+
+      fileReader.close();
+      System.out.println("\n - Successfully executed \'read\' process");
+
+    } catch (FileNotFoundException e) {
+      System.out.println("Error with \'read\' process");
+    }
+  }
 }
---
+++ src/main/java/com/quintrix/jfs/JDBCTest.java
@@ -0,0 +1,62 @@
+package com.quintrix.jfs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JDBCTest {
+
+  public static void main(String[] args) {
+    Connection connection = null;
+    Statement insertStmt = null;
+    Statement selectStmt = null;
+    Statement deleteStmt = null;
+    String separator = "----------------";
+
+    try {
+      // Connect to database
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      connection =
+          DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "Blackocean12!");
+
+      // Insert data into table
+      insertStmt = connection.createStatement();
+      insertStmt.execute(
+          "INSERT INTO city (Name,CountryCode,District,Population) VALUES ('San Angelo', 'USA', 'Texas', 100031)");
+      insertStmt.execute(
+          "INSERT INTO city (Name,CountryCode,District,Population) VALUES ('Big Spring', 'USA', 'Texas', 28041)");
+
+      // Select data from table
+      selectStmt = connection.createStatement();
+      ResultSet rs =
+          selectStmt.executeQuery("SELECT ID, Name,Population FROM city WHERE district = 'Texas'");
+
+      // Formated Header
+      System.out.printf("%-5s" + "|" + "%-30s" + "|" + "%-11s" + "%n", "ID", "Name", "Population");
+      System.out.println(separator + separator + separator);
+
+      while (rs.next()) {
+        System.out.printf("%-5s" + "|" + "%-30s" + "|" + "%-11s" + "%n", rs.getString(1),
+            rs.getString(2), rs.getInt(3));
+      }
+
+      // Delete data from tale
+      deleteStmt = connection.createStatement();
+      deleteStmt.execute("DELETE FROM city  WHERE Name = 'San Angelo'");
+      deleteStmt.execute("DELETE FROM city  WHERE Name = 'Big Spring'");
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        selectStmt.close();
+        insertStmt.close();
+        deleteStmt.close();
+        connection.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
